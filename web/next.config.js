4498c9d3a09bfb95a00eb833823b9f30c9dc55f1
Brent Robbins <brent@variantstudios.com>
2022-01-04 05:51:05 -0700

    Updated to use Preact vs just React

--- web/next.config.js
+++ web/next.config.js
@@ -1,6 +1,16 @@
-/** @type {import('next').NextConfig} */
+// module.exports = {
+//   swcMinify: true,
+//   productionBrowserSourceMaps: true,
+//   images: {
+//     domains: ['cdn.sanity.io',]
+//   }
+// }
 
-
+// https://nextjs.org/docs/advanced-features/security-headers
+// https://scotthelme.co.uk/a-new-security-header-referrer-policy/
+// https://stackoverflow.com/a/61846083
+// https://guydumais.digital/blog/how-to-deploy-a-strict-content-security-policy-csp-with-next-js/
+// https://content-security-policy.com/examples/allow-inline-style/
 const securityHeaders = [
   {
     key: 'X-DNS-Prefetch-Control',
@@ -28,25 +38,29 @@ const securityHeaders = [
   // }
 ]
 
-module.exports = {
-  reactStrictMode: true,
-  swcMinify: true,
-  // webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-  //   config.resolve.extensions = [ '.mjs', '.js', '.jsx', '.json', '.tsx', '.ts' ];
-  //   // Important: return the modified config
-  //   return config
-  // },
-  productionBrowserSourceMaps: true,
-  images: {
-    domains: ['cdn.sanity.io','images.pexels.com', 'segudpyruneoudwusaud.nhost.run', 's.gravatar.com', 'localhost',  'lh3.googleusercontent.com']
-  },
-  async headers() {
-    return [
-      {
-        // Apply these headers to all routes in your application.
-        source: '/(.*)',
-        headers: securityHeaders,
-      },
-    ]
-  }
-}
+// next.config.js
+const withPreact = require('next-plugin-preact');
+
+module.exports = withPreact({
+    /* regular next.js config options here */
+    swcMinify: true,
+    // webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    //   config.resolve.extensions = [ '.mjs', '.js', '.jsx', '.json', '.tsx', '.ts' ];
+    //   // Important: return the modified config
+    //   return config
+    // },
+    productionBrowserSourceMaps: true,
+    images: {
+      domains: ['cdn.sanity.io','images.pexels.com', 'segudpyruneoudwusaud.nhost.run', 's.gravatar.com', 'localhost',  'lh3.googleusercontent.com']
+    },
+    async headers() {
+      return [
+        {
+          // Apply these headers to all routes in your application.
+          source: '/(.*)',
+          headers: securityHeaders,
+        },
+      ]
+    }
+});
+
